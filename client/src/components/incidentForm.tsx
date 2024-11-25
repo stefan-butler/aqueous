e3bd2891c0d5f8674bde40ce8b6916ace8e87057
Alekos94 <alex.christodoulides94@gmail.com>
2024-11-25 14:25:16 +0000

    incident form handleChange

--- client/src/components/incidentForm.tsx
+++ client/src/components/incidentForm.tsx
@@ -1,19 +1,54 @@
-
+import { useState } from "react"
 
 function IncidentForm () {
+  interface Incident {
+    title: string;
+    incidentDate: string;
+    location: string; //we migth need to change this - depnding on how we will extract the location
+    severity: string;
+    floodType: string;
+    injuries: string; // maybe boolean
+    urgency: string;
+    name: string;
+    phone: string;
+    email:string;
+    additionalComments: string;
+  }
+  const [incident, setIncident] = useState<Incident>({
+    title:'',
+    incidentDate:'',
+    location:'',
+    severity:'',
+    floodType: '',
+    injuries: '',
+    urgency:'',
+    name:'',
+    phone:'',
+    email:'',
+    additionalComments: ''
+  })
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => {
+    const {name , value} = event.target;
+
+    setIncident ({...incident,
+      [name]: value
+    })
+  }
+
   return (
     <form className="flex flex-col">
       <label htmlFor='title' >Incident Title::</label>
-      <input type='text' name="title" id="title" placeholder="Incident Title"/>
+      <input type='text' name="title" id="title" placeholder="Incident Title" value={incident.title} onChange={handleChange}/>
 
       <label htmlFor='incidentDate'>Date & Time of Incident:</label>
-      <input type="datetime-local" name="incidentDate" id="incidentDate"/>
+      <input type="datetime-local" name="incidentDate" id="incidentDate" value={incident.incidentDate} onChange={handleChange}/>
 
       <label htmlFor="location">Location</label>
-      <input type="text" name="location" id="location" placeholder="Location"/>
+      <input type="text" name="location" id="location" placeholder="Location" value={incident.location} onChange={handleChange}/>
 
       <label htmlFor="severity">Severity of Flooding:</label>
-      <select name="severity" id="severity">
+      <select name="severity" id="severity" value={incident.severity} onChange={handleChange}>
         <option value='minor'>Minor</option>
         <option value='moderate'>Moderate</option>
         <option value='severe'>Severe</option>
@@ -21,7 +56,7 @@ function IncidentForm () {
       </select>
 
       <label htmlFor="floodType">Type of Flood:</label>
-      <select name="floodType" id="floodType">
+      <select name="floodType" id="floodType" value={incident.floodType} onChange={handleChange}>
         <option value="riverFlood">River Flood</option>
         <option value="urbanFlood">Urban Flood</option>
         <option value="flashFlood">Flash Flood</option>
@@ -32,14 +67,14 @@ function IncidentForm () {
       <label htmlFor="injuries">Injuries:</label>
       <div>
         <label htmlFor="injuriesYes">Yes</label>
-        <input type="radio" name="injuries" id="injuriesYes" value="Yes"/>
+        <input type="radio" name="injuries" id="injuriesYes" value="Yes" checked={incident.injuries === "Yes"} onChange={handleChange}/>
 
         <label htmlFor="injuriesNo">No</label>
-        <input type="radio" name="injuries" id="injuriesNo" value="No"/>
+        <input type="radio" name="injuries" id="injuriesNo" value="No" checked={incident.injuries === "No"} onChange={handleChange}/>
       </div>
 
       <label htmlFor="urgency">Urgency Level:</label>
-      <select name="urgency" id="urgency">
+      <select name="urgency" id="urgency" value={incident.urgency} onChange={handleChange}>
         <option value="immediate">Immediate</option>
         <option value="withinHours">Within Hours</option>
         <option value="within24Hours">Immediate</option>
@@ -47,16 +82,16 @@ function IncidentForm () {
       </select>
 
       <label htmlFor="name">Reporting Person's Name:</label>
-      <input type="text" name="name" id="name"/>
+      <input type="text" name="name" id="name" value={incident.name} onChange={handleChange}/>
 
       <label htmlFor="phone">Phone Number:</label>
-      <input type="tel" name="phone" id="phone"/>
+      <input type="tel" name="phone" id="phone" value={incident.phone} onChange={handleChange}/>
 
       <label htmlFor="email">Email Address:</label>
-      <input type="email" name="email" id="email"/>
+      <input type="email" name="email" id="email" value={incident.email} onChange={handleChange}/>
 
       <label htmlFor="additionalComments">Additional Comments:</label>
-      <textarea name="additionalComments" id="additionalComments"></textarea>
+      <textarea name="additionalComments" id="additionalComments" value={incident.additionalComments} onChange={handleChange}></textarea>
 
       <button type="submit">Submit Incident</button>
     </form>
