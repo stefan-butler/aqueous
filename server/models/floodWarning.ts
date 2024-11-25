a4e9e17e867bf76d4ccd84a05f8e9adbaa80ebf6
mjsscott <mjsscottmusic@gmail.com>
2024-11-25 13:00:15 +0000

    feat: modified data fetch and warning model

--- server/models/floodWarning.ts
+++ server/models/floodWarning.ts
@@ -1,23 +1,17 @@
 import mongoose, { Document, Schema } from "mongoose";
 
 interface  IFloodWarning extends Document {
-  polygon_id: string;
+  id: string;
   severity: number;
   message: string;
-  polygon: {
-    type: 'Polygon';
-    coordinates: number[][][];
-  };
+  floodArea: string;
 }
 
 const floodWarningSchema: Schema = new Schema({
-  polygon_id: { type: String, required: true, unique: true }, // Unique ID for the polygon
+  id: { type: String, required: true, unique: true }, // Unique ID for the polygon
   severity: { type: Number, required: true }, // Severity level (e.g., 1-5)
   message: { type: String, required: true }, // Message associated with the warning
-  polygon: {
-    type: { type: String, enum: ['Polygon'], required: true }, // GeoJSON type for Polygon
-    coordinates: { type: [[Number]], required: true }
-  }
+  floodAreaId: { type: String, require: true},
 });
 
 const FloodWarning = mongoose.model<IFloodWarning>('FloodWarning', floodWarningSchema);
--- server/services/fetchFloodData.ts
+++ server/services/fetchFloodData.ts
@@ -3,23 +3,19 @@ import FloodWarning from "../models/floodWarning";
 
 export const fetchAndStoreFloodData = async () => {
   try {
-    const response = await axios.get('https://environment.data.gov.uk/flood-monitoring/id/floods');
-    const data = response.data;
+    const response = await axios.get('http://environment.data.gov.uk/flood-monitoring/id/floods?min-severity=3');
+    const data = response.data.items;
     for (const item of data) {
       const id = item["@id"];
       const severity = item.severity || 4;
       const message = item.message || "No message";
-      const polygonUrl = item.floodArea.polygon;
-
-      const polygonResponse = await axios.get(polygonUrl);
-      const polygonData = polygonResponse.data;
-      const coordinates = polygonData.features[0].geometry.coordinates;
+      const floodArea = item.floodAreaId
 
       const floodWarning = new FloodWarning({
         polygon_id: id,
         severity: severity,
         message: message,
-        polygon: coordinates
+        floodArea: floodArea,
       });
 
       await floodWarning.save();
