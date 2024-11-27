fc8a37d44cc1d6fc8890f60540716835a3d9ca12
Stefan Butler <stefanbutler@icloud.com>
2024-11-27 15:06:43 +0000

    feat: outline chat page - note heavy work in progress

--- client/src/pages/chat.tsx
+++ client/src/pages/chat.tsx
@@ -1,8 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function Chat () {
+  const [messages, setMessages] = useState<any | null>(null);
+
+
+  async function fetchMessages() {
+    const messages = await axios(`http://localhost:3000/api/chat/${chatId}/messages}`);
+    setMessages(messages);
+  }
+
+  // handle Send 
+  function handleSend () {
+    
+  }
+
+  useEffect(() => {
+    fetchMessages();
+  }, []);
+
   return (
-    <h1>CHAT PAGE</h1>
+    <div>
+      <h1>CHAT</h1>
+      {/* messages */}
+      <div>
+        EXAMPLE MESSAGE
+      </div>
+      <label htmlFor='message-input'></label>
+      <input 
+        type='text'
+        id='message-input'
+        placeholder='your message here...'
+      />
+      <button
+      // onClick - post message
+      >SEND</button>
+    </div>
   )
 }
-
-export default Chat;
