3fe0297be3020362f59d09bd8d1175b450be75b1
mjsscott <mjsscottmusic@gmail.com>
2024-11-27 15:05:00 +0000

    feat: map-layer toggle

---
+++ client/src/redux/slices/mapLayerSlice.ts
@@ -0,0 +1,34 @@
+// redux/slices/mapLayerSlice.ts
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface LayerState {
+  layerId: string;
+  visibility: boolean;
+}
+
+interface MapLayersState {
+  layers: LayerState[];
+}
+
+const initialState: MapLayersState = {
+  layers: [
+    { layerId: 'heatmap', visibility: true },
+    { layerId: 'active-warnings', visibility: true },
+  ],
+};
+
+const mapLayerSlice = createSlice({
+  name: 'mapLayers',
+  initialState,
+  reducers: {
+    toggleLayer: (state, action: PayloadAction<string>) => {
+      const layer = state.layers.find(layer => layer.layerId === action.payload);
+      if (layer) {
+        layer.visibility = !layer.visibility;
+      }
+    },
+  },
+});
+
+export const { toggleLayer } = mapLayerSlice.actions;
+export default mapLayerSlice.reducer;
--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
+import mapLayerReducer from './slices/mapLayerSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    mapLayers: mapLayerReducer,
   },
 });
 
