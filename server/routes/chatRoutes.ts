96d767ccea5261d21c8e798aa96e81f0caffd3b9
Stefan Butler <stefanbutler@icloud.com>
2024-11-28 13:47:25 +0000

    feat: api route protection for chat feature

--- server/routes/chatRoutes.ts
+++ server/routes/chatRoutes.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import chatController from "../controllers/chatController";
+import requireAuth from "../middleware/requireAuth";
 
 const router = express.Router();
 
-router.get('/:responderId', chatController.getAllChats);
-router.post('/', chatController.createChat);
-router.get('/:chatId/messages', chatController.getMessages);
-router.post('/:chatId/messages', chatController.sendMessage);
+router.get('/:responderId', requireAuth, chatController.getAllChats);
+router.post('/', requireAuth, chatController.createChat);
+router.get('/:chatId/messages', requireAuth, chatController.getMessages);
+router.post('/:chatId/messages', requireAuth, chatController.sendMessage);
 
 export default router;
